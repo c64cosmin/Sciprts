e70c0b9988a4355938d47b8eb4ebf04db47caf70
c64cosmin <c64cosmin@gmail.com>
2017-08-11 11:49:30 -0700

    Always work with a config file

--- Linux/start_screens.sh
+++ Linux/start_screens.sh
@@ -1,17 +1,44 @@
-if [ "$1" = "SCREEN" ]; then
-$0 COMMAND "./start_api.sh" API
-$0 COMMAND "./start_conductor.sh" Conductor
-$0 COMMAND "tail -f /var/log/vdibroker/api.py.log" API_Logs
-$0 COMMAND "tail -f /var/log/vdibroker/Conductor.py.log" Conductor_Logs
+function AddScreenCommand {
+    screen -t "$2" bash
+    screen -X stuff "$1\n"
+}
+
+if [ "$2" = "%%%%%" ]; then
+    MAX_IT=`cat $1 | wc -l`
+    IT=2
+    while [ $IT -lt $MAX_IT ]; do
+        WindowName=`head -n $IT $1 | tail -n 1`
+        CommandLine=`head -n $(($IT + 1)) $1 | tail -n 1`
+        AddScreenCommand "$CommandLine" "$WindowName"
+        echo "$CommandLine" "$WindowName"
+        IT=$(($IT + 2))
+    done
 fi
 
-if [ "$1" = "COMMAND" ]; then
-    screen -t "$3" bash
-    screen -X stuff "$2\n"
+if [ -z "$1" ]; then
+    echo "Usage:"
+    echo "$0 [-h] file"
+    exit 0
+fi
+
+if [ "$1" = "-h" ]; then
+    echo "Example file:"
+    echo ""
+    echo "ScreenSessionName"
+    echo "Window Title 1"
+    echo "./command1.sh a b c"
+    echo "Window Title 2"
+    echo "./command2.sh abc"
+    exit 0
 fi
 
 screen --version > /dev/null
-if [ $? -ne 1 ]; then exit 1 ; fi
+if [ $? -ne 1 ]; then
+    echo "screen not installed"
+    exit 1
+fi
 screen -list | grep VDI > /dev/null
 if [ $? -ne 1 ]; then echo "Screen already exists" ; exit 1 ; fi
-screen -dmS VDI -s /bin/bash bash -c "$0 SCREEN" 
+
+SessionName=`head -n 1 $1`
+screen -dmS "$SessionName" -s /bin/bash bash -c "$0 $1 %%%%%"
