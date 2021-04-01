776d526e2d09115883710744d54cad42dab10c90
Cosmin Munteanu <cosmin1.munteanu@intel.com>
2021-04-02 02:03:50 +0300

    Update ycmsave

--- Linux/ycmsave.sh
+++ Linux/ycmsave.sh
@@ -4,13 +4,18 @@ function ycmsave(){
         return 1
     fi
 
-    if [ ! -f ".ycm_extra_conf.py" ]; then
-        echo "There is no .ycm_extra_conf.py file here"
+    if [ ! -f ".ycm_extra_conf.py" ] && [ ! -f "compile_commands.json" ]; then
+        echo "There is no .ycm_extra_conf.py or compile_commands.json file here"
         return 1
     fi
 
     CURRENT_FOLDER=`pwd`
 
     mkdir -p $YCM_EXTRA_CONF_FOLDER$CURRENT_FOLDER
-    cp .ycm_extra_conf.py $YCM_EXTRA_CONF_FOLDER$CURRENT_FOLDER
+    if [ -f ".ycm_extra_conf.py" ]; then
+        cp .ycm_extra_conf.py $YCM_EXTRA_CONF_FOLDER$CURRENT_FOLDER
+    fi
+    if [ -f "compile_commands.json" ]; then
+        cp compile_commands.json $YCM_EXTRA_CONF_FOLDER$CURRENT_FOLDER
+    fi
 }
