6d425ee37d1b9e340f93f837d7824d832eaa0271
c64cosmin <c64cosmin@gmail.com>
2021-04-07 00:28:54 +0300

    Proper scripts install, also add FixIt in YCM

--- Env/install.sh
+++ Env/install.sh
@@ -9,7 +9,7 @@ echo VIM = $VIM
 echo HOME = $HOME
 
 cp    Vim/vimrc           $VIM/vimrc
-cp -r Vim/scripts         $HOME/.vim/scripts
+cp -r Vim/scripts         $HOME/.vim
 cp -r Vim/colors          $VIM
 cp    Mintty/c64          /usr/share/mintty/themes
 cp    Home/.inputrc       ~
