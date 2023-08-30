c1c8ee01d624a70398da208dae70cd9de4ddf939
c64cosmin <c64cosmin@gmail.com>
2023-08-31 01:57:34 +0300

    And got rid of Linux too

--- Env/update.sh
+++ Env/update.sh
@@ -10,5 +10,8 @@ echo HOME = $HOME
 
 cp $VIM/vimrc                   Vim
 cp -r $HOME/.vim/scripts        Vim
-cp /usr/share/mintty/themes/c64 Mintty
 cp ~/.c64.bashrc                Home
+
+if [ "$OSTYPE" = "msys" ]; then
+    cp /usr/share/mintty/themes/c64 Mintty
+fi
